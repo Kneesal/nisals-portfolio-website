427dafd1f1765963a57e89ea8713f1d7df124d38
Kneesal <114973713+Kneesal@users.noreply.github.com>
2023-02-10 23:58:10 +1300

    re-worded cotnact Component

--- src/Component/Contact/ContactMe.js
+++ src/Component/Contact/ContactMe.js
@@ -2,14 +2,15 @@ import React from "react";
 import './contact.css'
 
 const ContactMe = () => {
+  
   return (
-    <div className="contactbar">
+    <div className="contactbar" >
       <h1 style = {{ paddingBottom: '3em'}} >Contact Me</h1>
       <ul className="footer" style = {{ paddingBottom: '5rem'}}>
         <li className="footer items">GitHub</li>
         <li className="footer items">LinkedIn</li>
         <li className="footer items">Email</li>
-        <li className="footer items">Resume/CV</li>
+        <li className="footer items">Resume</li>
       </ul>
     </div>
   );
