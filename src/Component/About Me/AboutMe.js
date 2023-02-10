2320ca0bf0bc8ed974bfe7a181b22ac97e68eaf5
Kneesal <114973713+Kneesal@users.noreply.github.com>
2023-02-10 19:06:00 +1300

    improved css

--- src/Component/About Me/AboutMe.js
+++ src/Component/About Me/AboutMe.js
@@ -3,11 +3,11 @@ import './aboutme.css'
 
 const AboutMe = () => {
   return (
-    <div style = {{ paddingBottom: '5rem'}}>
+    <div style = {{ paddingBottom: '13rem'}}>
       <h1 className="aboutheader" style = {{ paddingBottom: '3em'}}>About Me</h1>
         <div className="aboutme flex">
-            <div>
-                <img style = {{borderRadius: "1.5em", width: '400px', height: 'auto' }} alt = 'profilePhoto'src="https://gweb-research-imagen.web.app/compositional/An%20oil%20painting%20of%20a%20British%20Shorthair%20cat%20wearing%20a%20cowboy%20hat%20and%20red%20shirt%20skateboarding%20on%20a%20beach./1_.jpeg"></img>
+            <div style = {{ width: '30rem'}}>
+                <img style = {{borderRadius: "100rem", width: '100%', height: 'auto' }} alt = 'profilePhoto'src="https://gweb-research-imagen.web.app/compositional/An%20oil%20painting%20of%20a%20British%20Shorthair%20cat%20wearing%20a%20cowboy%20hat%20and%20red%20shirt%20skateboarding%20on%20a%20beach./1_.jpeg"></img>
             </div>
             <div style = {{ overflowWrap: "break-word", width: '400px'}}>
                 <p> Enter text here about yourself. As you can see I am a skateboarding cat. Not a lot much else going on, wbu?</p>
--- src/Component/Projects/Projects.js
+++ src/Component/Projects/Projects.js
@@ -7,31 +7,30 @@ const Projects = () => {
             <div className="">
             <h1 style = {{ paddingBottom: '3em'}}>Projects</h1>
                 <div className="project-flex-wrapper">
-                    <div>
-                        <img src="https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png" style = {{ width: '500px', height: 'auto', borderRadius: "1.5em"}} alt = {'project'}></img>
-                    </div>
                     <div style = {{ overflowWrap: "break-word", width: '400px'}}>
                          <p> this is my project dawg this is my project dawgthis is my project dawgthis is my project dawgthis is my project dawgthis is my project dawgthis is my project dawgthis is my project dawgthis is my project dawg</p>
                     </div>
-                </div> 
-                <div className="project-flex-wrapper">
-                    <div>
-                        <img src="https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png" style = {{ width: '500px', height: 'auto', borderRadius: "1.5em"}} alt = {'project'}></img>
-                    </div>
-                    <div style = {{ overflowWrap: "break-word", width: '400px'}}>
-                         <p> this is my project dawg</p>
+                    <div style = {{ width: '45rem', }}>
+                        <img src="https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png" alt = {'project'} style = {{ width: '100%', height: 'auto', borderRadius: '2rem'}}></img>
                     </div>
                 </div> 
                 <div className="project-flex-wrapper">
-                    <div>
-                        <img src="https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png" style = {{ width: '500px', height: 'auto', borderRadius: "1.5em"}} alt = {'project'}></img>
-                    </div>
                     <div style = {{ overflowWrap: "break-word", width: '400px'}}>
-                         <p> this is my project dawg</p>
+                         <p> this is my project dawg this is my project dawgthis is my project dawgthis is my project dawgthis is my project dawgthis is my project dawgthis is my project dawgthis is my project dawgthis is my project dawg</p>
+                    </div>
+                    <div style = {{ width: '45rem', }}>
+                        <img src="https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png" alt = {'project'} style = {{ width: '100%', height: 'auto', borderRadius: '2rem'}}></img>
+                    </div>
+                </div> 
+                                <div className="project-flex-wrapper">
+                    <div style = {{ overflowWrap: "break-word", width: '400px'}}>
+                         <p> this is my project dawg this is my project dawgthis is my project dawgthis is my project dawgthis is my project dawgthis is my project dawgthis is my project dawgthis is my project dawgthis is my project dawg</p>
+                    </div>
+                    <div style = {{ width: '45rem', }}>
+                        <img src="https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png" alt = {'project'} style = {{ width: '100%', height: 'auto', borderRadius: '2rem'}}></img>
                     </div>
                 </div> 
             </div>
-
         </div>
     )
 }
