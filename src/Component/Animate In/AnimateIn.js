84a9a896537c56e834640c88c2ae20ac44f47cda
Kneesal <114973713+Kneesal@users.noreply.github.com>
2023-02-25 00:11:02 +1300

    added AnimateIn Component with animations library

---
+++ src/Component/Animate In/AnimateIn.js
@@ -0,0 +1,74 @@
+import {useRef, useEffect, useState} from "react";
+
+const AnimateIn = ({from, to, children}) => {
+
+    const ref = useRef(null)
+    const [isIntersecting, setIsIntersecting] = useState(false);
+    
+        const options = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.5
+          }
+
+        useEffect(()=> {
+            const element = ref.current
+            const observer = new IntersectionObserver(([entry])=> setIsIntersecting(entry.isIntersecting),options);
+            if(element){observer.observe(element)}
+
+            return () => {
+                if(element){observer.unobserve(element)}
+            }
+        })
+
+    const transition = {transition: 'all 0.5s ease-in-out', width: '100%'}
+    return(
+        <div
+        style={ isIntersecting ? {
+            ...to,
+            ...transition,
+        }: {
+            ...from,
+            ...transition
+        }} 
+        ref = {ref}>
+
+            {children}
+
+        </div>
+    )
+}
+
+const FadeUp = ({children}) => {
+
+    return(
+    <AnimateIn from = {{opacity: 0, translate:'0 20px' }} to = {{opacity: 1, translate: 'none'}}>
+      {children}
+    </AnimateIn>
+    )
+  };
+  
+const FadeRight = ({children}) => {
+
+    return(
+    <AnimateIn from = {{opacity: 0, translate:'20px 0px' }} to = {{opacity: 1, translate: 'none'}}>
+      {children}
+    </AnimateIn>
+    )
+  }
+
+const FadeLeft = ({children}) => {
+
+    return(
+    <AnimateIn from = {{opacity: 0, translate:'-20px 0px' }} to = {{opacity: 1, translate: 'none'}}>
+      {children}
+    </AnimateIn>
+    )
+  }
+
+
+export const Animate = {
+    FadeUp,
+    FadeLeft,
+    FadeRight
+};
