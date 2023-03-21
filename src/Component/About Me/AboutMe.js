025595d65d52af252402d63872a878fb22b0b3c8
Kneesal <114973713+Kneesal@users.noreply.github.com>
2023-03-22 07:52:05 +1300

    added assets folder & changed contactMe footer animation to FadeIn

--- src/Component/About Me/AboutMe.js
+++ src/Component/About Me/AboutMe.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './aboutme.css'
 import { Animate } from '../Animate In/AnimateIn.js'
+import profilepic from '../../assets/profilepic.jpeg'
 
 const AboutMe = () => {
 
@@ -18,7 +19,7 @@ const AboutMe = () => {
                 <img 
                 style = {{borderRadius: "100rem", width: '100%', height: 'auto'}}
                 alt = 'profilePhoto'
-                src="https://scontent.fakl1-4.fna.fbcdn.net/v/t1.6435-9/67543874_2416105221768880_2313676067488923648_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=TGxa4aW8IhIAX_OdQxE&_nc_ht=scontent.fakl1-4.fna&oh=00_AfBRANNaPVLyD_muZ119tfXUj2ejOlAGrP5DJvmXkGYhag&oe=6413D61E">  
+                src={profilepic}>  
                 </img>
             </Animate.FadeUp>
             </div>
--- src/Component/Contact/ContactMe.js
+++ src/Component/Contact/ContactMe.js
@@ -6,9 +6,12 @@ import resume from '../../Resume/Nisals-Resume.pdf'
 const ContactMe = () => {
 
   return (
-    <Animate.FadeUp>
+    
       <div className="contact-container" id="contact">
+        <Animate.FadeUp>
             <h1 style = {{ padding: '1em'}} >Contact Me</h1>
+        </Animate.FadeUp>
+          <Animate.FadeUp>
             <div className="contact-wrapper">
                 <p className="cta-text">Lets work together!</p>
                 <a
@@ -18,15 +21,17 @@ const ContactMe = () => {
                   href="mailto:nisalcottingham@gmail.com"
                   >Call to Action</a>
             </div>
+            </Animate.FadeUp>
             <div style = {{ paddingTop: '3em'}}>
+            <Animate.FadeIn>
               <ul className="footer">
                 <li className="footer items"><a href="https://github.com/Kneesal"  target="_blank" rel="noopener noreferrer"><img src="githubicon.svg" height='30px' width= '30px' alt="github"></img></a></li>
                 <li className="footer items"><a href="https://www.linkedin.com/in/nisal-cottingham-8b660216b/?originalSubdomain=nz"  target="_blank" rel="noopener noreferrer"><img src="linkedinicon.svg" height='30px' width= '30px' alt="linkedin"></img></a></li>
                 <li className="footer items"><a href={resume} target='_blank' rel="noopener noreferrer"><img src="resumeicon.png" height='30px' width= '30px' alt="resume"></img></a></li>
               </ul>
+              </Animate.FadeIn>
             </div>
       </div>
-      </Animate.FadeUp>
   );
 };
 
